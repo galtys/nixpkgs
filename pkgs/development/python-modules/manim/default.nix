a24c1378a6f2225e5fbf7463c539f0b99c86c218
Jan Troler <Jan Troler>
2025-06-08 15:18:12 +0200

    added dataclasses_json to manim

--- pkgs/development/python-modules/manim/default.nix
+++ pkgs/development/python-modules/manim/default.nix
@@ -34,6 +34,7 @@
   rich,
   scipy,
   sympy,
+  dataclasses_json,
   screeninfo,
   skia-pathops,
   srt,
@@ -234,6 +235,7 @@ buildPythonPackage rec {
     pygments
     rich
     scipy
+    dataclasses_json
     sympy
     screeninfo
     skia-pathops
